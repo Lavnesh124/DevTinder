49ebd5c7ebb878f502fba3e69387c6b0f0989e86
Lavnesh yadav <Lavneshy@gmail.com>
2026-02-09 22:27:55 +0530

    create login API

--- src/app.js
+++ src/app.js
@@ -35,6 +35,24 @@ app.post('/signup', async (req,res)=>{
 
 });
 
+app.post('/login',async (req,res)=>{
+    const { email,password } = req.body;
+    try{
+        const user= await User.findOne({email:email});
+        if(!user){
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const isPasswordValid=await bcrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+        res.json({ message: 'Login successful' });
+    }
+    catch(err){
+        console.error('Error during login:', err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
 
 //Get user by email
 app.get('/users', async (req,res)=>{
